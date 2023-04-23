0a760f6650ad74af02aa728a4e4c18953d5804df
unknown <shagunk035@gmail.com>
2023-04-23 18:57:46 +0530

    Initial commit

---
+++ app/src/main/java/com/example/taskcompleted/MainActivity.kt
@@ -0,0 +1,98 @@
+package com.example.taskcompleted
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.taskcompleted.ui.theme.TaskCompletedTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TaskCompletedTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ComposeCompletedApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ComposeCompletedApp(){
+    Completed(
+        painter = painterResource(id = R.drawable.ic_task_completed), 
+        firstLine = stringResource(id = R.string.first_line), 
+        secondLine = stringResource(id = R.string.second_line))
+}
+
+@Composable
+fun Completed( painter: Painter, firstLine: String, secondLine: String, modifier: Modifier = Modifier){
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painter,
+            contentDescription = null
+        )
+        Text(
+            text = firstLine,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(
+                    top = 24.dp,
+                    bottom = 8.dp
+                )
+        )
+        Text(
+            text = secondLine,
+            fontSize = 16.sp
+        )
+
+    }
+
+}
+
+
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun GreetingPreview() {
+    TaskCompletedTheme {
+        ComposeCompletedApp()
+    }
+}
